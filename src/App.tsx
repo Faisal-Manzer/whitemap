5e330f370535829ed3b587f492b5ed5cb4e7e1a4
Faisal Manzer <faisal_manzer@yahoo.in>
2024-11-02 07:10:32 +0530

    feat: experiment with line drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,105 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { MouseEvent, useEffect, useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const mouseRef = useRef({
+    x: 0,
+    y: 0,
+
+    oldX: 0,
+    oldY: 0,
+
+    draw: false,
+  });
+
+  const setup = () => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ratio = Math.ceil(window.devicePixelRatio);
+    canvas.width = window.innerWidth * ratio;
+    canvas.height = window.innerHeight * ratio;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+    ctx.scale(ratio, ratio);
+    ctx.setTransform(ratio, 0, 0, ratio, 0, 0);
+    ctx.imageSmoothingEnabled = true;
+    ctx.imageSmoothingQuality = "high";
+
+    canvas.style.backgroundColor = "#ddd";
+  };
+
+  const onMouseDown = (e: MouseEvent<HTMLCanvasElement>) => {
+    mouseRef.current.draw = true;
+  };
+
+  const onMouseMove = (e: MouseEvent<HTMLCanvasElement>) => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const rect = canvas.getBoundingClientRect();
+    mouseRef.current = {
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top,
+
+      oldX: mouseRef.current.x,
+      oldY: mouseRef.current.y,
+
+      draw: mouseRef.current.draw,
+    };
+  };
+
+  const onMouseUp = () => {
+    mouseRef.current.draw = false;
+  };
+
+  useEffect(() => {
+    let loopId: number;
+    const loop = () => {
+      loopId = requestAnimationFrame(loop);
+      const canvas = canvasRef.current;
+      const ctx = canvas?.getContext("2d");
+      if (!canvas || !ctx) return;
+
+      canvas.style.cursor = "default";
+
+      const mouse = mouseRef.current;
+      if (!mouse.draw) return;
+
+      canvas.style.cursor = "crosshair";
+      ctx.beginPath(); // begin
+
+      ctx.lineWidth = 5;
+      ctx.lineCap = "round";
+      ctx.strokeStyle = "#c0392b";
+
+      ctx.moveTo(mouse.oldX, mouse.oldY); // from
+      ctx.lineTo(mouse.x, mouse.y);
+
+      ctx.stroke();
+    };
+
+    setup();
+    loop();
+    return () => {
+      cancelAnimationFrame(loopId);
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <canvas
+        ref={canvasRef}
+        style={{ height: "100vh", width: "100vw", margin: 0, padding: 0 }}
+        onMouseDown={onMouseDown}
+        onMouseMove={onMouseMove}
+        onMouseUp={onMouseUp}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
