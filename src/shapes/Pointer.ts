64fe3cf97017518ca7f944e0406d7f7020f0f3dd
Faisal Manzer <faisal_manzer@yahoo.in>
2024-11-03 08:25:51 +0530

    fix: pointer style

--- src/shapes/Pointer.ts
+++ src/shapes/Pointer.ts
@@ -21,6 +21,10 @@ export class Pointer extends Shape {
 
     this.start = null;
     this.end = null;
+
+    this.config.borderWidth = 0.5;
+    this.config.border = "#7c3aed";
+    this.config.background = 'rgba(221, 214, 254, 0.5)';
   }
 
   move(e: MouseEvent<HTMLCanvasElement>): void {
@@ -34,18 +38,16 @@ export class Pointer extends Shape {
   draw(ctx: OffscreenCanvasRenderingContext2D): void {
     if (!this.start || !this.end) return;
 
-    ctx.beginPath();
-    ctx.lineWidth = 5;
-    ctx.lineCap = "round";
-    ctx.strokeStyle = "#3b82f6";
-    ctx.fillStyle = "#3b82f622"
-
+    this.configure(ctx);
+    ctx.setLineDash([15, 5]);
     ctx.rect(
       this.start.x,
       this.start.y,
       this.end.x - this.start.x,
       this.end.y - this.start.y,
     );
+
+    ctx.fill();
     ctx.stroke();
   }
 }
